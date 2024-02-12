a45c49b7dcddc14177c85eff3ca2ae60273005df
Deniz Acay <me@denizacay.com>
2024-02-12 20:16:35 +0300

    Tekrar 1

--- src/main/java/tr/workintech/courses/egzersiz/DosyaIslemleri.java
+++ src/main/java/tr/workintech/courses/egzersiz/DosyaIslemleri.java
@@ -5,13 +5,16 @@ import java.nio.file.Path;
 
 public class DosyaIslemleri {
 
-    private static String dosya = "";
+    private static String dosya = "/Users/deniz/Projects/Workintech/workintech-java/content.txt";
 
     public static void main(String[] args) {
 
         if (!Files.exists(Path.of(dosya))) {
 
             System.out.println("Dosyaya erisilemiyor.");
+        } else {
+
+            System.out.println("Dosya bulundu.");
         }
     }
 }
