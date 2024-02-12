cc1f603ed52ac43fbb4c5e736b0684a0890ebe63
Deniz Acay <me@denizacay.com>
2024-02-12 20:49:29 +0300

    Dosya okuma yontemleri

--- src/main/java/tr/workintech/courses/egzersiz/DosyaIslemleri.java
+++ src/main/java/tr/workintech/courses/egzersiz/DosyaIslemleri.java
@@ -1,13 +1,15 @@
 package tr.workintech.courses.egzersiz;
 
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class DosyaIslemleri {
 
     private static String dosya = "/Users/deniz/Projects/Workintech/workintech-java/content.txt";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         if (!Files.exists(Path.of(dosya))) {
 
@@ -16,5 +18,46 @@ public class DosyaIslemleri {
 
             System.out.println("Dosya bulundu.");
         }
+
+        String dosyaIcerigi = dosyaOku();
+
+        System.out.println(dosyaIcerigi);
+    }
+
+    private static String dosyaOku() throws IOException {
+
+        File targetFile = new File(dosya);
+
+        // 1. yontem - BufferedReader
+
+//        BufferedReader bufferedReader = new BufferedReader(new FileReader(targetFile));
+//
+//        StringBuilder tumIcerik = new StringBuilder();
+//
+//        String st;
+//
+//        while((st = bufferedReader.readLine()) != null) { // null gelene kadar oku demek
+//
+//            System.out.println(st);
+//
+//            tumIcerik.append(st);
+//        }
+//
+//        return tumIcerik.toString();
+
+        // 2. yontem -> Scanner
+
+        Scanner sc = new Scanner(targetFile);
+
+        StringBuilder tumIcerik = new StringBuilder();
+
+        while(sc.hasNextLine())
+            tumIcerik.append(sc.nextLine());
+
+        return tumIcerik.toString();
+
+        // 3. yontem
+
+//        return Files.readString(Path.of(dosya));
     }
 }
